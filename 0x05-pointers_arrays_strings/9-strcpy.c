f4a0eddd67f415572625ea2dd3da250d0df8ea79
Mhyro <jerryscarry@gmail.com>
2022-09-20 21:42:28 -0700

    d

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int inc = 0;
 
-	while (*(src + inc) != '\0')
-	{
+		while (*(src + inc) != '\0')
+		{
 			*(dest + inc) = *(src + inc);
 			inc++;
-	}
+		}
 	*(dest + inc) = '\0';
 
 
